c640e9e29d48b9750c4aeead345794cbc94700a1
rahul <rh377162@dal.ca>
2024-07-10 12:33:05 -0300

    TF script updated

--- K8s/gkeshell.tf
+++ K8s/gkeshell.tf
@@ -1,4 +1,3 @@
-# Configure the Google Cloud provider
 provider "google" {
   project = "cloud-427713"
   region  = "us-central1"
@@ -10,11 +9,19 @@ resource "google_container_cluster" "primary" {
 
   deletion_protection = false
   node_config {
-        machine_type = "e2-micro"
-        disk_type = "pd-standard"
-        disk_size_gb = 10
-        image_type = "COS_CONTAINERD"
-        oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+         machine_type = "e2-medium"
+          disk_type    = "pd-balanced"
+          disk_size_gb = 100
+          image_type   = "COS_CONTAINERD"
+          oauth_scopes = [
+            "https://www.googleapis.com/auth/cloud-platform",
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+            "https://www.googleapis.com/auth/service.management.readonly",
+            "https://www.googleapis.com/auth/servicecontrol",
+            "https://www.googleapis.com/auth/trace.append",
+          ]
     }
 
     initial_node_count = 1
